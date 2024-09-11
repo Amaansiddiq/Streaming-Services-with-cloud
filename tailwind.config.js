087ee02ef8f1f9a3c4ba411dd3caa44a504df441
Siddiqui Amaan <90834290+Amaansiddiq@users.noreply.github.com>
2024-09-12 00:09:45 +0530

    Updated files

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/templates/**/*.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black-dark': '#00000050',
+        'dull-white': '#FFFFFFB3',
+        'white-light': '#FFFFFF30',
+        'white-medium': '#FFFFFF40',
+        'neon-blue': '#2FB8FF',
+        'neon-red': '#FF5733',
+      },
+      fontFamily: {
+        'nunito': ['nunito', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
